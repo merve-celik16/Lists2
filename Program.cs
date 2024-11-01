d4ef633530223e0743a1ace3ef83115b5215ea4f
merve-celik16 <mervedursunn34@gmail.com>
2024-11-01 12:53:26 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lists2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Boş bir liste oluşturalım
+            List<string> kahveListesi = new List<string>();
+
+            // Kullanıcıdan 5 kahve ismi alalım
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($" Kahve {i + 1} : ");
+                string kahveIsmi = Console.ReadLine();
+                kahveListesi.Add(kahveIsmi); // Kahve ismi ekleniyor
+            }
+
+            // Oluşturulan listeyi ekrana yazdıralım
+            Console.WriteLine("\nKahve listeniz:");
+            foreach (string kahve in kahveListesi)
+            {
+                Console.WriteLine(kahve);
+            }
+        }
+    }
+}
